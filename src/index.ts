09a0649e2c3ec1f3db8c38824cfdd9b939c8ba40
gaurav <guru@odrways.com>
2023-05-29 01:40:25 +0530

    python package path issue fixed

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,46 @@
-import { spawn } from "child_process";
+import { spawn, ChildProcess, StdioOptions } from "child_process";
 import path from "path";
 
-export function HtmlToPdf(html: string, filePath: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const pythonProcess = spawn("python3", [
-      path.join(__dirname, "python/html_to_pdf.py"),
-      html,
-      filePath,
-    ]);
-
-    pythonProcess.stderr.on("data", (data: any) => {
-      reject(new Error(`Child process stderr: ${data}`));
-    });
-
-    pythonProcess.on("close", (code: any) => {
-      if (code === 0) {
-        resolve("PDF generated successfully");
-      } else {
-        reject(new Error(`Child process exited with code ${code}`));
+export async function HtmlToPdf(
+  html: string,
+  filePath: string
+): Promise<string> {
+  try {
+    const pythonProcess: ChildProcess = spawn(
+      "python3",
+      [
+        process.cwd() + "/node_modules/html-toolkit/python/html_to_pdf.py",
+        html,
+        filePath,
+      ],
+      {
+        stdio: ["pipe", "pipe", "pipe"] as StdioOptions, // add type to stdio options
       }
-    });
+    );
 
-    pythonProcess.on("error", (err: any) => {
-      reject(new Error(`Failed to start child process: ${err}`));
+    if (pythonProcess.stderr !== null) {
+      // add null check for stderr using '!=='
+      pythonProcess.stderr.on("data", (data: Buffer) => {
+        throw new Error(`Child process stderr: ${data.toString()}`);
+      });
+    } else {
+      throw new Error("Child process has no stderr property.");
+    }
+
+    return new Promise((resolve, reject) => {
+      pythonProcess.on("close", (code: number) => {
+        if (code === 0) {
+          resolve("PDF created successfully.");
+        } else {
+          reject(new Error(`Child process exited with code ${code}`));
+        }
+      });
+
+      pythonProcess.on("error", (err: Error) => {
+        reject(new Error(`Failed to start child process: ${err.message}`));
+      });
     });
-  });
+  } catch (error) {
+    throw error;
+  }
 }
