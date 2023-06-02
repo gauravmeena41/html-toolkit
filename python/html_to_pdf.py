788208a22a3720489de2b5db890f7e415048154f
gaurav <guru@odrways.com>
2023-06-02 22:44:01 +0530

    added wkhtmltopdf-binary package

--- python/html_to_pdf.py
+++ python/html_to_pdf.py
@@ -2,15 +2,10 @@ import pdfkit
 import sys
 import subprocess
 
-
-path_wkhtmltopdf = subprocess.run(
-    ["which", "wkhtmltopdf"], stdout=subprocess.PIPE, check=True).stdout.decode().strip()
-config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-
 options = {
     'page-size': 'Letter',
 }
 
 
 pdf_bytes = pdfkit.from_string(
-    sys.argv[1], output_path=sys.argv[2], configuration=config, options=options, verbose=True)
+    sys.argv[1], output_path=sys.argv[2], options=options, verbose=True)
